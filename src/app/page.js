788750e6f85d19b2e6f758d3443c4cf6c33c5afa
Oleg David Remennyk <olegbro123@gmail.com>
2024-07-31 20:15:05 +0300

    added styling background and basic form/card component

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,16 @@
 "use client"
 import Image from "next/image";
 import { run } from "./prompt";
+import Card from "./components/Card";
 
 export default function Home() {
-  function handleForm(e){
-    e.preventDefault()
-    const name = e.target[0].value
-    const experience = e.target[1].value
-    const skills = e.target[2].value
-    const input = `my name ${name}, I'm from Pakistan, I am 27 years old, i have experience in ${experience} my main skills are ${skills} `
-    run(input)
-  }
+ 
+  
   return (
-    <form className="flex flex-col w-48" onSubmit={(e)=>handleForm(e)}>
-      <label>name</label>
-      <input name="name" className="border border-black"/>
-      <label>experience</label>
-      <input name="experience" className="border border-black"/>
-      <label>skills</label>
-      <textarea name="skills" className="border border-black"/>
-      <button className="border border-black" type="submit">submit</button>
-    </form>
+    <>
+    
+    <Card></Card>
+    
+    </>
   );
 }
