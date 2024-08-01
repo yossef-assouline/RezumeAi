5463963d4ca6ee8e2be23bc99bcb1ef49fbaf5c6
Oleg David Remennyk <olegbro123@gmail.com>
2024-08-01 19:32:34 +0300

    Added Intro/about/Payment components with basic skeleton

---
+++ src/app/components/about.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Image from "next/image";
+
+export const About = () => {
+  return (
+    <>
+    <div className="flex justify-evenly items-center ">
+      <img className="rounded-full" src="https://placehold.co/800x600" alt="" />
+      <p className="text-2xl w-[20vw]">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+        optio eaque maxime natus nisi debitis quisquam! Perferendis et deserunt
+        consequuntur veniam incidunt id delectus, architecto ullam fugiat nobis
+        repudiandae vitae.
+      </p>
+      </div>
+    </>
+  );
+};
--- src/app/page.js
+++ src/app/page.js
@@ -3,14 +3,18 @@ import Image from "next/image";
 import { run } from "./prompt";
 import Card from "./components/Card";
 import { NavBar } from "./components/NavBar";
-import { Hero } from "./components/Hero";
+import { Intro } from "./components/Intro";
+import { About } from "./components/about";
+import { Payment } from "./components/Payment";
 export default function Home() {
  
   
   return (
     <>
     <NavBar/>
-    <Hero/>
+    <Intro/>
+    <About/>
+    <Payment/>
     </>
   );
 }
