535d1edc19d7b8797946b6c594531d3be4c5a661
Yossef <chipeurrr@gmail.com>
2024-09-08 20:58:50 +0300

    added styled-components and fixed bug

--- src/app/page.js
+++ src/app/page.js
@@ -4,19 +4,13 @@ import { run } from "./prompt";
 import UserInfo from "./components/UserInfo";
 import { NavBar } from "./components/NavBar";
 import { Hero } from "./components/Hero";
-import { About } from "./components/about";
-import { Payment } from "./components/Payment";
-import Education from "./components/Education";
-export default function Home() {
- 
-  
+
+export default function Home(){
   return (
     <>
     <NavBar/>
-    <Intro/>
-    <About/>
-    <Card/>
-    <Payment/>
+    <Hero/>
     </>
-  );
+    
+    )
 }
